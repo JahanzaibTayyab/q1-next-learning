11008b85071ff413fa2502bc7864bf7efedfcccb
Jahanzaib Tayyab <jtayyab204@gmail.com>
2023-05-22 10:31:38 +0500

    update changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import jahanzaibImage from "@/assets/images/Jahanzaib.jpg";
 
 export default function Home() {
   return (
-    <div className="flex justify-center bg-gray-300">
-      <div className="mx-4 order-last self-center">
+    <div className="flex justify-center bg-gray-100 py-5">
+      <div className="mx-4 order-last self-center ">
         <Image
           src={jahanzaibImage}
           alt="image"
-          width={100}
-          height={100}
-          className="rounded-full w-100 h-100 object-contain"
+          width={96}
+          height={96}
+          className="rounded-full w-24 h-24 object-fill"
         />
       </div>
       <div>
-        <h1>Welcome to NorthBy</h1>
+        <h1>Jahanzaib Tayyab</h1>
         <h2 className="text-6xl font-bold text-yellow-700">
-          A premium in sight and sound
+          Full Stack Developer
         </h2>
         <button className="mx-4 my-8 px-4 py-2 border-2 border-black rounded-lg text-white bg-blue-900 ">
           Learn More
--- src/views/header/index.tsx
+++ src/views/header/index.tsx
@@ -2,12 +2,22 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <nav className="flex gap-6 py-4 px-5 bg-gray-300">
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact-us">Contact Us</Link>
-      <Link href="/dashboard">Dashboard</Link>
-      <Link href="/blogs">Blogs</Link>
+    <nav className="flex gap-6 py-2 px-5 bg-gray-300">
+      <Link href="/" className="text-sm">
+        Home
+      </Link>
+      <Link href="/about" className="text-sm">
+        About
+      </Link>
+      <Link href="/contact-us" className="text-sm">
+        Contact Us
+      </Link>
+      <Link href="/dashboard" className="text-sm">
+        Dashboard
+      </Link>
+      <Link href="/blogs" className="text-sm">
+        Blogs
+      </Link>
     </nav>
   );
 };
